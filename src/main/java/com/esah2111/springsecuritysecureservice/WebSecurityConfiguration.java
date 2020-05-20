2cb3d127c68a1475c27edbf582cb57f1f142ae2c
Mohammad Esah <esah2111@gmail.com>
2020-05-20 19:58:27 +0530

    Initial Commit

---
+++ src/main/java/com/esah2111/springsecuritysecureservice/WebSecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.esah2111.springsecuritysecureservice;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration {
+
+}
---
+++ src/test/java/com/esah2111/springsecuritysecureservice/SpringsecuritySecureserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esah2111.springsecuritysecureservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritySecureserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
